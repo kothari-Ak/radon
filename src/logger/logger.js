d2096ce2788b242dfe6d6b6d8f86e881ab3a7da6
kothari-Ak <kotharianjali13@gmail.com>
2022-06-03 06:04:48 +0530

    updated assignment2

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -3,5 +3,18 @@
 const welcome=function() {
     console.log("Welcome to my application. I am Anjali and a part of FunctionUp Radon cohort.")
 }
-
 module.exports.welcome=welcome
+
+const array=function(array,size){
+    let a=[]
+    for (let i=0;i<array.length;i+=size){
+    
+        const ch= array.slice(i, i+size)
+        console.log(ch)   
+    }
+ return a
+    }
+    module.exports.array=array
+
+    
+
--- src/router/route.js
+++ src/router/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const externalModule = require('../logger/logger')
 const dateModule=require('../helper')
 const caseModule=require('../formatter')
-
+const lodash=require('lodash')
 const router = express.Router();
 
 
@@ -16,6 +16,27 @@ router.get('/test-me', function (req, res){
      caseModule.changeToUpperCase()
      res.send('my first ever api')
 })
+
+router.get('/hello',function(req, res){
+   
+   const tail=lodash.tail([1,3,5,7,9,11,13,15,17,19]);
+   console.log(tail)
+
+   const chunk=lodash.chunk(["January","February","March","April","May","June","July","August","September","October","November","December"],3)
+   console.log(chunk)
+
+   const merge=lodash.union([6,9,4],[4,9,4],[3,7,5],[2,7,1],[0,8,1])
+   console.log(merge)
+
+   const pairs=[["name","Anjali"],["age",22]]
+   const z=lodash.fromPairs(pairs)
+   console.log(z)
+
+   res.send("hello")
+})
+
+
+
 
 module.exports = router;
 // adding this comment for no reason
