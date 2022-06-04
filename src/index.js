1571079aaac3485483078434672e32d040636385
kothari-Ak <kotharianjali13@gmail.com>
2022-06-04 11:55:29 +0530

    ass git-api

--- src/index.js
+++ src/index.js
@@ -5,12 +5,11 @@ const route = require('./router/route.js');
 
 const app = express();
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', route);
 
-app.listen(process.env.PORT || 5000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 5000))
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
--- src/router/route.js
+++ src/router/route.js
@@ -1,21 +1,64 @@
 const express = require('express');
-const externalModule = require('../logger/logger')
-const dateModule=require('../helper')
-const caseModule=require('../formatter')
+// const myHelper = require('../util/helper')
+//const underscore = require('underscore')
 
 const router = express.Router();
 
-
-router.get('/test-me', function (req, res){
-     externalModule.welcome()
-     dateModule.printDate()
-     dateModule.printMonth()
-     dateModule.getBatchInfo()
-     caseModule.trim()
-     caseModule.changeToLowerCase()
-     caseModule.changeToUpperCase()
-     res.send('my first ever api')
+router.get('/movie', function(req, res){
+   // console.log('The request objects is '+ JSON.stringify(req.params))
+   movie=["shreekrishna","dhoom","afsana","gehraiyan"]
+   res.send(movie)})
+   //for(indexnumber=0;indexnumber<movies.length;indexnumber++){   
+   
+ router.get('/movies/:indexnumber',function(req,res) {
+   movies=["shreekrishna","dhoom","afsana","gehraiyan"]
+   if(req.params.indexnumber<movies.length===true)
+   {
+      console.log(movies[req.params.indexnumber]+' index number is '+req.params.indexnumber)
+   }
+   else{
+   res.send("Error:Use a valid index")}
+   
 })
 
-module.exports = router;
-// adding this comment for no reason
+
+router.get('/films',function(req,res) {
+   const films=[
+      {
+     "id":1,
+     "name":"shreekrishna"
+  },
+  {
+     "id":2,
+     "name":"dhoom",
+  },
+  {  "id":3,
+     "name":"afsana"
+  },
+  {  "id":4,
+     "name":"gehraiyan"}]
+   res.send(films)})
+
+
+router.get('/films/:filmId',function(req,res) {
+   const films=[
+      {
+     "id":1,
+     "name":"shreekrishna"
+  },
+  {
+     "id":2,
+     "name":"dhoom",
+  },
+  {  "id":3,
+     "name":"afsana"
+  },
+  {  "id":4,
+     "name":"gehraiyan"}]
+
+   let obj=films[req.params.filmId-1]
+   if(obj!==films.length){
+      res.send(obj||"No movie exists with this is id")
+   }
+})
+   module.exports = router;
