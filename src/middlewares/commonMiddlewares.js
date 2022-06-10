4468df3e33fdf3375557a21078fa530a23124bf7
kothari-Ak <kotharianjali13@gmail.com>
2022-06-10 20:53:25 +0530

    middleware assignment 1

--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,26 +1,35 @@
+const{next_id}=require("process")
 
-const mid1= function ( req, res, next) {
-    req.falana= "hi there. i am adding something new to the req object"
-    console.log("Hi I am a middleware named Mid1")
+function midware(req,res,next){
+    let currentTime =new Date
+    let ip=req.ip
+    let url=req.protocol +'://'+req.get('host')+ req.originalUrl
+    console.log(`${currentTime},${ip} , ${url}`)
     next()
 }
 
-const mid2= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid2")
-    next()
-}
+// const midware= function ( req, res, next) {
+//     req.body= "hi there. i am adding something new to the req object"
+//     console.log("This is Anjali")
+//     next()
+// }
 
-const mid3= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid3")
-    next()
-}
+// const mid2= function ( req, res, next) {
+//     console.log("Hi I am a middleware named Mid2")
+//     next()
+// }
 
-const mid4= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid4")
-    next()
-}
+// const mid3= function ( req, res, next) {
+//     console.log("Hi I am a middleware named Mid3")
+//     next()
+// }
 
-module.exports.mid1= mid1
-module.exports.mid2= mid2
-module.exports.mid3= mid3
-module.exports.mid4= mid4
+// const mid4= function ( req, res, next) {
+//     console.log("Hi I am a middleware named Mid4")
+//     next()
+// }
+
+module.exports.midware=midware
+// module.exports.mid2= mid2
+// module.exports.mid3= mid3
+// module.exports.mid4= mid4
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,17 +2,18 @@ const express = require('express');
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+// const BookController= require("../controllers/bookController")
 const commonMW = require ("../middlewares/commonMiddlewares")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+router.get("/timestamp", commonMW.midware ,UserController.timestamps)
 
 
 
 
-router.post("/createBook", BookController.createBook  )
+
+
+
+// router.post("/createBook", BookController.createBook  )
 
 
 
@@ -48,7 +49,7 @@ router.post("/createBook", BookController.createBook  )
 
 
 
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
+// router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
 
 
 
