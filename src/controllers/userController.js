cdfbb28dec77369d3ddaa6dd56c1ae965a670c26
kothari-Ak <kotharianjali13@gmail.com>
2022-06-17 15:53:04 +0530

    auth 1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,7 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
+const midware=require("../midware");
+const { send } = require("process");
 
 const createUser = async function (abcd, xyz) {
   //You can name the req, res objects anything.
@@ -31,39 +33,37 @@ const loginUser = async function (req, res) {
   let token = jwt.sign(
     {
       userId: user._id.toString(),
-      batch: "thorium",
+      batch: "radon",
       organisation: "FunctionUp",
     },
     "functionup-radon"
   );
   res.setHeader("x-auth-token", token);
-  res.send({ status: true, token: token });
+   res.send({ status: true, token: token });
 };
 
 const getUserData = async function (req, res) {
-  let token = req.headers["x-Auth-token"];
-  if (!token) token = req.headers["x-auth-token"];
-
-  //If no token is present in the request header return error
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-
-  console.log(token);
+ 
+res.send({msg:token})
+  
   
   // If a token is present then decode the token with verify function
   // verify takes two inputs:
   // Input 1 is the token to be decoded
   // Input 2 is the same secret with which the token was generated
   // Check the value of the decoded token yourself
-  let decodedToken = jwt.verify(token, "functionup-radon");
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
+  // let decodedToken = jwt.verify(token, "functionup-radon");
+  // console.log(decodedToken)
+  // if (!decodedToken)
+  //   return res.send({ status: false, msg: "token is invalid" });
+    
 
-  let userId = req.params.userId;
-  let userDetails = await userModel.findById(userId);
-  if (!userDetails)
-    return res.send({ status: false, msg: "No such user exists" });
+  // let userId = req.params.userId;
+  // let userDetails = await userModel.findById(userId);
+  // if (!userDetails)
+  //   return res.send({ status: false, msg: "No such user exists" });
 
-  res.send({ status: true, data: userDetails });
+  // res.send({ status: true, data: userDetails });
 };
 
 const updateUser = async function (req, res) {
